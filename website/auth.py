eb1b64304875f0a64bfc965d6f7f84e680d7b61e
DanteChoppaXXX <only1dantechoppa@gmail.com>
2024-02-15 04:40:02 -0500

    update

--- website/auth.py
+++ website/auth.py
@@ -4,7 +4,12 @@ auth = Blueprint('auth', __name__)
 
 @auth.route('/login', methods=["POST", "GET"])
 def login():
-    return render_template("login.html")
+    if request.method == "POST":
+        data = request.form
+        print(data)
+        return render_template("home.html")
+    else:
+        return render_template("login.html")
 
 @auth.route('/logout')
 def logout():
@@ -13,6 +18,11 @@ def logout():
 @auth.route('/signup', methods=["POST", "GET"])
 def signup():
     if request.method == "POST":
+        email = request.form.get("email")
+        firstName = request.form.get("firstName")
+        password1 = request.form.get("password1")
+        password2 = request.form.get("password2")
+        
         return render_template("login.html")
     else:
         return render_template("signup.html")
